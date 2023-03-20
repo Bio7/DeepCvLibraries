4cac8706e5b55177486835a570bad308defafbc0
Bio7 <marcel.au@web.de>
2023-03-20 16:40:21 +0100

    Added a Maven repo to fill a plugin with external maven libs.

---
+++ com.eco.bio7.libs.deepcv/src/main/java/com/eco/bio7/libs/maven/App.java
@@ -0,0 +1,13 @@
+package com.eco.bio7.libs.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
